291bd2df49bde6fef2515fbd0a34c956b3cb7eb8
Adam Vo <adam.vo@ubiquid.fr>
2023-12-26 23:14:12 +0100

    Reset / Styles / Font

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./assets/styles/reset.css";
+import "./assets/fonts/almamono/stylesheet.css";
+import "./assets/styles/variables.css";
+import "./assets/styles/main.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
